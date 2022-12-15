194af95395face1f3a8027f506f736dde4bf6b36
Keegan Farkas <85268378+KeeganFarkas@users.noreply.github.com>
2022-12-15 14:19:41 -0500

    Initial Commit
    Runtime: 49ms - beats 95.4%
    Memory: 14.1MB - beats 94.83%

---
+++ RansomNote.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        for i in ransomNote:
+            flag: bool = True
+
+            for j in magazine:
+                if i is j:
+                    magazine = magazine.replace(j, '', 1)
+                    flag = False
+                    break
+            
+            if flag:
+                return False
+        return True
